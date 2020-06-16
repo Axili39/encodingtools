fec2bc12ea5d8b3ca2579f841a2f44d1dd7dfe27
Axili39 <Axili39@github.com>
2020-06-16 09:26:03 +0000

    added test & github task

---
+++ encodingtools_test.go
@@ -0,0 +1,43 @@
+package encodingtools
+
+import (
+	"os"
+	"testing"
+)
+
+func TestConvertion(t *testing.T) {
+	json := `{"A":1,"F":1.2,"O":{"foo":"bar"},"S":"foo","T":[1,2,3]}`
+	yaml := `A: 1
+F: 1.2
+O:
+    foo: bar
+S: foo
+T:
+  - 1
+  - 2
+  - 3
+`
+	// Nominal JSON2Yaml
+	data, err := JSON2Yaml([]byte(json))
+
+	if err != nil {
+		t.Errorf("JSON2Yaml: error unexpected %v", err)
+	}
+	if string(data) != yaml {
+		t.Errorf("JSON2Yaml: bad yaml format \n%s vs \n%s", string(data), yaml)
+	}
+
+	// Nominal Yaml2JSON
+	data, err = Yaml2JSON([]byte(yaml))
+
+	if err != nil {
+		t.Errorf("Yaml2JSON: error unexpected %v", err)
+	}
+	if string(data) != json {
+		t.Errorf("Yaml2JSON: bad yaml format \n%s vs \n%s", string(data), json)
+	}
+}
+
+func TestMain(m *testing.M) {
+	os.Exit(m.Run())
+}
